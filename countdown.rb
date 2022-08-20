f95a0dddb3e78d66a1edbe2baac3833a885773c0
bonveeee <bonochieng@gmail.com>
2022-08-20 06:44:56 +0300

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,24 @@
 #write your code here
+  require 'pry'
 
-def countdown
+def countdown(int)
+    i = int
+    while i > 0
+          puts   "#{i} SECOND(S)!"
+               i -= 1
+                     
+    end     
+    "HAPPY NEW YEAR!"
+end
+puts countdown(10)
+
+
+def countdown_with_sleep(int)
+    i = int
+    while i > 0
+          puts   "#{i} SECOND(S)!"
+               i -= 1
+               sleep 1                     
+    end     
+    "HAPPY NEW YEAR!"
 end
